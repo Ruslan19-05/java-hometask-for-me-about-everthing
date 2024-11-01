791934eed843bfb42949e5770a5ba557accb2ba9
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-11-01 22:02:18 +0400

    loops problems solving

---
+++ src/main/java/az/edu/turing/LoopApp3.java
@@ -0,0 +1,13 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class LoopApp3 {
+
+    public static void main(String[]args){
+        //input
+        Scanner scan =new Scanner(System.in);
+
+
+    }
+}
