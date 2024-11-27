95c026df0059a745fbb8a34d542a84dda51c0b9b
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-11-27 20:22:02 +0400

    fregef

--- src/main/java/az/edu/turing/Lessons/FiveTaskApp.java
+++ src/main/java/az/edu/turing/Lessons/FiveTaskApp.java
@@ -7,31 +7,19 @@ public class FiveTaskApp {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int count= scanner.nextInt();
-        int[] n= new int[count];
+        if(count>=2) {
+            double[] salary = new double[count];
 
-        if(count >=2) {
-            inputWorker(n);
-            checkBig(inputWorker(n));
-        }
-
-    }
-
-
-    public static int[] inputWorker(int[] n){
-        Scanner scanner=new Scanner(System.in);
-        if(n.length>=2){
-            int [] worker=new int[n.length];
-            for(int i=0;i<n.length;i++){
-                   worker[i]= scanner.nextInt();
+            for (int i=0;i<count;i++){
+                salary[i]= scanner.nextInt();
             }
-        }else {
-            System.out.print("Wrong num");
-        }
-        return n;
-    }
+            checkBig(salary);
 
-    public static void checkBig(int[] worker){
-        int max=worker[0];
+
+        }
+    }
+    public static void checkBig(double[] worker){
+        double max=worker[0];
         for(int i=1;i<worker.length;i++){
             if(max<worker[i]){
                 max=worker[i];
